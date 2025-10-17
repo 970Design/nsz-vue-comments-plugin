71d63b75c8985640d0e1909cf7176ec9ac73d3e5
TJ Szewczak <tj.dividemysky@gmail.com>
2025-10-17 08:26:08 -0600

    remove update checker

--- nsz-vue-comments.php
+++ nsz-vue-comments.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: 970 Design Headless Comments
  * Description: Secure proxy endpoints for headless WordPress comments with Akismet spam protection and reCaptcha support.
- * Version:     1.2.2
+ * Version:     1.2.3
  * Author:      970 Design
  * Author URI:  https://970design.com/
  * License:     GPLv2 or later
